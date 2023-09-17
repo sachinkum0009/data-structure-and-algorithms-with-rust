c7a17b7d3876890b1a2af9939fd6664516eef9ff
Sachin Kumar <sachinkum123567@gmail.com>
2023-09-17 23:42:53 +0200

    :gun: borrowing and ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -22,4 +22,52 @@ fn main() {
 
     println!("{}, world!", s2);
 
+    let sb = String::from("hello");
+
+    // let (sb2, len) = calculate_length(sb);
+    let len = calculate_length(&sb);
+
+    println!("The length of '{}', is {}", sb, len);
+
+    let mut sc = String::from("hello");
+
+    let r1 = &mut sc;
+    // let r2 = &mut sc;
+
+    // println!("{}, {}", r1, r2);
+
+    change(&mut sc);
+
+    println!("sc is {}", sc);
+
+
+    let mut sd = String::from("hello world");
+    let world = first_word(&s);
+
+    s.clear();
+
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+// fn calculate_length(s: String) -> (String, usize) {
+//     let length = s.len();
+//     (s, length)
+// }
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+    s.len()
 }
