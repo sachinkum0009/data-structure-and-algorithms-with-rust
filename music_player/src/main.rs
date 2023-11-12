1a1882e05303f90589f3653f5c06ea3e5457695b
Sachin Kumar <sachinkum123567@gmail.com>
2023-11-12 18:03:10 +0100

    music player cargo added

--- music_player/src/main.rs
+++ music_player/src/main.rs
@@ -1,3 +1,25 @@
+struct MyStruct {
+    a: u8,
+    b: u8,
+    c: u8
+}
+
+impl MyStruct {
+    fn add(&self) -> u8 {
+        self.a + self.b + self.c
+    }
+
+    fn mul(&self) -> u8 {
+        self.a * self.b
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Music Player");
+    
+    let my_struct = MyStruct{a: 10, b: 10, c: 10};
+    
+    println!("Value of a is {}", my_struct.a);
+    println!("Addition is {}", my_struct.add());
+    println!("Multition is {}", my_struct.mul());
 }
