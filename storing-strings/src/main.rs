aac6141990bd934e3948b0e8366ff76b8743dd9a
Sachin Kumar <sachinkum123567@gmail.com>
2023-12-01 23:17:41 +0100

    :gun: error handling

--- storing-strings/src/main.rs
+++ storing-strings/src/main.rs
@@ -1,7 +1,36 @@
-use std::io;
+use std::io::{self, Read};
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::ErrorKind;
+use std::net::IpAddr;
+
+
+
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+
+fn read_username_from_file2() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
 
 fn main() {
 
@@ -73,6 +102,18 @@ fn main() {
         },
     };
 
+    // let new_greeting_file = File::open("new_hello.txt")
+    //     .expect("new_hello.txt file doesn't exit");
 
+
+    // propagating errors
+
+
+
+    let home: IpAddr = "127.0.0.1"
+    .parse()
+    .expect("Hardcoded IP address should be valid");
+
+    println!("home ip is {}", home);
 
 }
